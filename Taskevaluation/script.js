fd2e7c122bb458b28c5b25a8438408b6d40791dd
Sonjoy209164 <sonjoy209164@gmail.com>
2025-10-14 17:31:08 +0600

    feat: "task evaluation"

--- Taskevaluation/script.js
+++ Taskevaluation/script.js
@@ -5,9 +5,20 @@ const projects = [
     link: "https://sonjoy209164.github.io/Code-Marshal-Learning/"
   },
   {
-    name: "Task Day 3 - CSS Variables",
+    name: " Image Processing app-CSS variables",
     link: "https://sonjoy209164.github.io/Code-Marshal-Learning/Task%20Day3-JavaScript/Javascript30/03%20-%20CSS%20Variables/index.html"
   },
+    {
+    name: " Drum Kit",
+    link: "https://sonjoy209164.github.io/Code-Marshal-Learning/Task%20Day4-javaScript30/01%20-%20JavaScript%20Drum%20Kit/index-START.html"
+  },
+     {
+    name: " Alarm clock - Time ",
+    link: "https://sonjoy209164.github.io/Code-Marshal-Learning/Task%20Day4-javaScript30/02%20-%20JS%20and%20CSS%20Clock/index-START.html"
+  },
+  
+
+  
   // Add more manually as needed
 ];
 
